1cc273de21ffd9f85e6a75bb3d1eb4fdf7826af1
idris kulubi <137810838+169398@users.noreply.github.com>
2023-12-29 01:29:56 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,7 @@ import '@/styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: 'Crow',
 }
 
 export default function RootLayout({
