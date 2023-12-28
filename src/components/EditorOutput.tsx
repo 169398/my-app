63e3519d4f95881db6d2aa1480509f9e33e8f6a1
idris kulubi <137810838+169398@users.noreply.github.com>
2023-12-29 02:55:22 +0300

    Update EditorOutput.tsx

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,41 +1,36 @@
-'use client'
+import React, { FC, Suspense } from "react";
+import CustomCodeRenderer from "@/components/renderers/CustomCodeRenderer";
+import CustomImageRenderer from "@/components/renderers/CustomImageRenderer";
 
-import CustomCodeRenderer from '@/components/renderers/CustomCodeRenderer'
-import CustomImageRenderer from '@/components/renderers/CustomImageRenderer'
-import { FC } from 'react'
-import dynamic from 'next/dynamic'
-
-const Output = dynamic(
-  async () => (await import('editorjs-react-renderer')).default,
-  { ssr: false }
-)
+const Output = React.lazy(() => import("editorjs-react-renderer"));
 
 interface EditorOutputProps {
-  content: any
+  content: any;
 }
 
 const renderers = {
   image: CustomImageRenderer,
   code: CustomCodeRenderer,
-}
+};
 
 const style = {
   paragraph: {
-    fontSize: '0.875rem',
-    lineHeight: '1.25rem',
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
   },
-}
+};
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
   return (
-    // @ts-expect-error
-    <Output
-      style={style}
-      className='text-sm'
-      renderers={renderers}
-      data={content}
-    />
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Output
+        style={style}
+        className="text-sm"
+        renderers={renderers}
+        data={content}
+      />
+    </Suspense>
+  );
+};
 
-export default EditorOutput
+export default EditorOutput;
