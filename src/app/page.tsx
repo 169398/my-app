791d7b6038f1a3e3f1f242b1eba9ae054eb8e3c9
idris kulubi <137810838+169398@users.noreply.github.com>
2023-12-29 12:44:16 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
 
         {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
-          <div className='bg-emerald-100 px-6 py-4'>
+          <div className='bg-gradient-to-r from-blue-400 to-emerald-400 px-6 py-4'>
             <p className='font-semibold py-3 flex items-center gap-1.5'>
               <HomeIcon className='h-4 w-4' />
               Home
