06c11c38a9231f6c8525b5aef0016c45f01b2cdb
idris kulubi <137810838+169398@users.noreply.github.com>
2023-12-31 17:53:07 +0300

    Update page.tsx
    Changed temporarily for a logedin user to access general feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
       <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
         {/* @ts-expect-error server component */}
-        {session ? <CustomFeed /> : <GeneralFeed />}
+        {session ? <GeneralFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
